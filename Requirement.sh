53ca68e8d839c177f2a74d2dec7aedf93a97801b
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-02-04 22:00:17 +0330

    Create Requirement.sh

---
+++ Requirement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# macOS
+if [ "$(uname)" == "Darwin" ]; then
+    if ! command -v brew &> /dev/null; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install openblas
+    brew install opencv
+    brew install cmake
+# linux
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get update
+    sudo apt-get install -y libopencv-dev
+    sudo apt-get install -y cmake
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
+
+pip install py-agender face_recognition
